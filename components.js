62155c8a3c0eea741e199fc2e7d820ab821c2606
Rob Hoes <rob.hoes@citrix.com>
2014-06-03 16:08:31 +0100

    First iteration of the XenAPI docs for the master branch

---
+++ components.js
@@ -0,0 +1,3 @@
+executables = ['mpathalert', 'gen_api_main', 'perftest', 'xapi', 'xe', 'block_device_io', 'v6d', 'datamodel']
+libraries = ['client', 'stats', 'common', 'server', 'vm_memory_constraints']
+packages = ['xapi-client']
